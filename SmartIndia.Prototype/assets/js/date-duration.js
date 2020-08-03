4e36272fd08a7b9932ead3c9584d3ba0e0914c83
Lipi <lipi55@gitlab.com>
2020-08-03 16:05:09 +0530

    Added super admin panel and thank you page and some design chnages.

---
+++ SmartIndia.Prototype/assets/js/date-duration.js
@@ -0,0 +1,9 @@
+$("#edatepicker").change(function () {
+    var startDate = $("#sdatepicker").val();
+    var endDate = $("#edatepicker").val();
+
+    var durationInTime = new Date(new Date(endDate) - new Date(startDate));
+    var durationInDays = durationInTime / 1000 / 60 / 60 / 24;
+
+    $("#durationDate").val(Math.round(durationInDays));
+});
