bb21c60f78e83c08469c1cb8eb3fd63746185b95
Napoleon Mohanta <napoleon.mohanta@gmail.com>
2020-08-04 18:13:12 +0530

    added .gitignore

--- SmartIndia.Data/Models/AuthenticateResponse.cs
+++ SmartIndia.Data/Models/AuthenticateResponse.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using SmartIndia.Data.Entities;
+using SmartIndia.Data.Entities.UserManagement;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,21 +9,21 @@ namespace SmartIndia.Data.Models
 {
     public class AuthenticateResponse
     {
-        public int Id { get; set; }
+        public Int64 Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Username { get; set; }
+        public string EmailID { get; set; }
         public string JwtToken { get; set; }
 
         [JsonIgnore] // refresh token is returned in http only cookie
         public string RefreshToken { get; set; }
 
-        public AuthenticateResponse(User user, string jwtToken, string refreshToken)
+        public AuthenticateResponse(UserRegistration userRegistration, string jwtToken, string refreshToken)
         {
-            Id = user.Id;
-            FirstName = user.FirstName;
-            LastName = user.LastName;
-            Username = user.Username;
+            Id = userRegistration.UserId;
+            FirstName = userRegistration.FirstName;
+            LastName = userRegistration.LastName;
+            EmailID = userRegistration.EmailId;
             JwtToken = jwtToken;
             RefreshToken = refreshToken;
         }
---
+++ SmartIndia.Data/Services/UserRegistrationServices.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using SmartIndia.Data.Entities.UserManagement;
+using SmartIndia.Data.Factory;
+using SmartIndia.Data.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Text;
+
+namespace SmartIndia.Data.Services
+{
+    public class UserRegistrationServices : RepositoryBase
+    {
+        string retMsg = string.Empty;
+        public UserRegistrationServices(IConnectionFactory connectionFactory) : base(connectionFactory)
+        {
+
+        }
+        public string UserRegestrationAction(UserRegistration registration)
+        {
+            var keyNew = Helper.GeneratePassword(10);
+            var password = Helper.EncodePassword(registration.Password, keyNew);
+            object[] objArray = new object[] {
+                     "@P_ACTIONCODE", registration.ACTIONCODE
+                    ,"@FirstName", registration.FirstName
+                    ,"@LastName", registration.LastName
+                    ,"@EmailId", registration.EmailId
+                    ,"@MobileNo", registration.MobileNo
+                    ,"@Password", password
+                    ,"@VCode", keyNew
+                    ,"@UID", Guid.NewGuid()
+                    ,"@MobileCountryCode", registration.MobileCountryCode
+                    ,"@UserNo", ""
+                    ,"@UpdatedById", string.Empty 
+            };
+            try
+            {
+                DynamicParameters param = objArray.ToDynamicParameters("@PVCH_MSGOUT");
+                param.Add("@UserTimeZone", DateTime.UtcNow, DbType.DateTime, ParameterDirection.Input);
+                var result = DBConnection.Execute("USP_UserRegistrations_ACTION", param, commandType: CommandType.StoredProcedure);
+                retMsg = param.Get<string>("PVCH_MSGOUT");
+
+            }
+            catch (Exception ex)
+            {
+                // throw new Exception(ex.Message);
+                retMsg = "400";
+               // log.Error(ex);
+            }
+            return retMsg;
+        }
+    }
+}
