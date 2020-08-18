75ee255c5cf17ba2536f934610cce7c9f19ae337
Kshetra <kshetra09@gitlab.com>
2020-08-18 14:31:16 +0530

    added course page

--- SmartIndia.WebApp/wwwroot/assets/js/date-duration.js
+++ SmartIndia.WebApp/wwwroot/assets/js/date-duration.js
@@ -7,3 +7,15 @@ $("#edatepicker").change(function () {
 
     $("#durationDate").val(Math.round(durationInDays));
 });
+
+$("#sdatepicker").change(function () {
+    var startDate = $("#sdatepicker").val();
+    var endDate = $("#edatepicker").val();
+
+    var durationInTime = new Date(new Date(endDate) - new Date(startDate));
+    var durationInDays = durationInTime / 1000 / 60 / 60 / 24;
+
+    $("#durationDate").val(Math.round(durationInDays));
+});
+
+
