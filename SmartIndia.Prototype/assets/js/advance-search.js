e4d841c84410f874e0eca797e69b6b6b81a385e8
Lipi <lipi55@gitlab.com>
2020-09-19 11:09:56 +0530

    Change the front panel design

---
+++ SmartIndia.Prototype/assets/js/advance-search.js
@@ -0,0 +1,24 @@
+﻿//Advance Search
+$(".advanceSearch").click(function () {
+    $("#advanceSearchArea").slideToggle("2000");
+});
+
+// When the user clicks anywhere outside of the search, close it
+$(document).mouseup(function (e) {
+    var container = $("#advanceSearchArea");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+
+//Clear Filter
+$("#clearfilter").click(function () {
+    $("#advanceSearchArea :input").val("");
+});
+
+//Clear Filter
+$("#clearfilter").click(function () {
+    $("#outer-advanceSearchArea :input").val("");
+});
