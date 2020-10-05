0ecea14d74b4d214aba748694933fafe9d673ee0
Napoleon Mohanta <napoleon.mohanta@gmail.com>
2020-10-05 17:26:47 +0530

    added back panel menu active

--- SmartIndia.WebApp/wwwroot/ApplicationScripts/Host/UpdateUserRoleId.js
+++ SmartIndia.WebApp/wwwroot/ApplicationScripts/Host/UpdateUserRoleId.js
@@ -2,14 +2,17 @@
     var UId = localStorage.getItem("userID");
     $.ajax(
         {
-            url: ServiceURL + "/api/AttendeeClassWall/UpdateUserRole?Userid=" + parseInt(UId)+"",
+            url: ServiceURL + "/api/AttendeeClassWall/UpdateUserRole?Userid=" + parseInt(UId) + "",
             type: "POST",
             dataType: "json",
             contentType: "application/json",
             success: function (data) {
                 if (data == "1") {
-                    window.location.href='/Hosts/HostDashboard/Dashboard';
-                   
+                    $.post("/ManageUsers/Users/AthenticationUserRole?UserId=" + UId + "&RoleName=Host", function (data) {
+                        if (data == 1) {
+                            window.location.href = "/Hosts/HostDashboard/Dashboard";
+                        }
+                    });
                 }
                 else {
                     BootstrapAlert('Something went wrong. Please try again');
