623fe532364dfc355ebed0c3789b740a7b86081e
Kshetra <kshetra09@gitlab.com>
2020-10-31 18:17:08 +0530

    Added DeviceResources

--- SmartIndia.WebApp/Areas/Hosts/Controllers/CoursesController.cs
+++ SmartIndia.WebApp/Areas/Hosts/Controllers/CoursesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SmartIndia.WebApp.Models;
@@ -9,6 +10,7 @@ using SmartIndia.WebApp.Models;
 namespace SmartIndia.WebApp.Areas.Hosts.Controllers
 {
     [Area("Hosts")]
+    [Authorize(Roles = "Host")]
     public class CoursesController : Controller
     {
         public IActionResult HostDashboard()
