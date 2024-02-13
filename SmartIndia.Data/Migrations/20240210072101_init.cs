5d1c8acaefcc3084b78ea6c96f5a50ed35574fa0
Jagadish Mohanty <j.or.mohanty@gmail.com>
2024-02-13 10:34:39 +0530

    Initial Commit

---
+++ SmartIndia.Data/Migrations/20240210072101_init.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartIndia.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRegistrations",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    ProfileName = table.Column<string>(nullable: true),
+                    EmailId = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    MobileNo = table.Column<string>(nullable: true),
+                    MobileConfirmed = table.Column<bool>(nullable: false),
+                    Password = table.Column<string>(nullable: true),
+                    VCode = table.Column<string>(nullable: true),
+                    UID = table.Column<Guid>(nullable: false),
+                    UserTimeZone = table.Column<DateTime>(nullable: false),
+                    MobileCountryCode = table.Column<string>(nullable: true),
+                    SignUpBy = table.Column<int>(nullable: false),
+                    UserNo = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    UpdatedById = table.Column<long>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<bool>(nullable: false),
+                    ServiceURL = table.Column<string>(nullable: true),
+                    RoleId = table.Column<int>(nullable: false),
+                    RoleName = table.Column<string>(nullable: true),
+                    DeviceResources = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRegistrations", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRegistrations_RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Token = table.Column<string>(nullable: true),
+                    Expires = table.Column<DateTime>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    CreatedByIp = table.Column<string>(nullable: true),
+                    Revoked = table.Column<DateTime>(nullable: true),
+                    RevokedByIp = table.Column<string>(nullable: true),
+                    ReplacedByToken = table.Column<string>(nullable: true),
+                    UserRegistrationUserId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRegistrations_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRegistrations_RefreshTokens_UserRegistrations_UserRegistrationUserId",
+                        column: x => x.UserRegistrationUserId,
+                        principalTable: "UserRegistrations",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users_RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Token = table.Column<string>(nullable: true),
+                    Expires = table.Column<DateTime>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    CreatedByIp = table.Column<string>(nullable: true),
+                    Revoked = table.Column<DateTime>(nullable: true),
+                    RevokedByIp = table.Column<string>(nullable: true),
+                    ReplacedByToken = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_RefreshTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRegistrations_RefreshTokens_UserRegistrationUserId",
+                table: "UserRegistrations_RefreshTokens",
+                column: "UserRegistrationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RefreshTokens_UserId",
+                table: "Users_RefreshTokens",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRegistrations_RefreshTokens");
+
+            migrationBuilder.DropTable(
+                name: "Users_RefreshTokens");
+
+            migrationBuilder.DropTable(
+                name: "UserRegistrations");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
